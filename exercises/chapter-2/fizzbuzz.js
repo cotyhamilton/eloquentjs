8f640d004216ef100ebb6af47884b1c791806b16
Coty Hamilton <cotyhamilton@gmail.com>
2016-08-23 20:06:02 -0500

    initial

---
+++ exercises/chapter-2/fizzbuzz.js
@@ -0,0 +1,16 @@
+for (var i = 1; i <= 100; i++) {
+    var fb = "";
+    if (i % 3 === 0) {
+        fb += "Fizz";
+    }
+    if (i % 5 === 0) {
+        fb += "Buzz";
+    }
+    console.log(fb || i)
+}
+
+/* ----- OFFICIAL SOLUTION ----- *
+
+SAME AS ABOVE
+
+ * ----------------------------- */
