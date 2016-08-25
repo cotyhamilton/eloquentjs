75393f786676a96b031f7eb8d26f55b2b18a71a4
Coty Hamilton <cotyhamilton@gmail.com>
2016-08-25 00:02:08 -0500

    initial

---
+++ exercises/chapter-3/bean-counting.js
@@ -0,0 +1,10 @@
+function countChars(str, char) {
+    var count = 0;
+    for (var i = 0; i < str.length; i++) {
+        if (str.charAt(i) === char)
+            count++;
+    }
+    return count;
+}
+
+console.log(countChars("Mississippi", "i"));
---
+++ exercises/chapter-3/minimum.js
@@ -0,0 +1,5 @@
+function min(first, second) {
+    return first < second ? first : second;
+}
+
+console.log(min(3,-4));
